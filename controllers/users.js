c9a3b52051d1ba7a846210e4a21ed71c4c671379
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-08-03 15:14:39 +0600

    исправлена логика update user

--- controllers/users.js
+++ controllers/users.js
@@ -4,30 +4,7 @@ const User = require('../models/user');
 const ConflictError = require('../errors/ConflictError');
 const ValidationError = require('../errors/ValidationError');
 const NotFoundError = require('../errors/NotFoundError');
-
-// const secretKey = process.env.SECRET_KEY; -- не могу иначе пройти автотесты
-
-// eslint-disable-next-line arrow-body-style
-const updateUserFields = (userId, updateFields) => {
-  return User.findByIdAndUpdate(userId, updateFields, { new: true, runValidators: true })
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
-      }
-      return user;
-    });
-};
-
-// module.exports.getUser = (req, res, next) => {
-//   User.findById((req.params.userId))
-//     .then((user) => {
-//       if (!user) {
-//         throw new NotFoundError('Запрашиваемый пользователь не найден');
-//       }
-//       res.send(user);
-//     })
-//     .catch((err) => next(err));
-// };
+const BadRequestError = require('../errors/BarRequestError');
 
 module.exports.createUser = (req, res, next) => {
   const {
@@ -52,9 +29,18 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.updateUser = (req, res, next) => {
   const { name, email } = req.body;
-  updateUserFields(req.user._id, { name, email })
+  User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
+    .orFail(() => next(new NotFoundError('Пользователь по указанному _id не найден.')))
     .then((user) => res.send(user))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('При обновлении профиля произошла ошибка.'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже существует.'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports.login = (req, res, next) => {
